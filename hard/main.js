7e7b7b26bfa66908590c39a64228b1204bb45f8e
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-06-05 20:02:58 +0300

    Add hard task and change ES6 to ES7 in .jshinrc

---
+++ hard/main.js
@@ -0,0 +1,16 @@
+let num = 266219,
+  divider = 10,
+  mathProduct = 1;
+  
+for (let i = 0; i < String(num).length; i++) {
+  mathProduct *= Math.floor((num % divider / (divider / 10)));
+  divider *= 10;
+}
+
+// Выводы произведения цифр числа в num
+console.log(mathProduct);
+// Возведение в степень
+mathProduct **= 3; // В стандарте ES6 выдаёт предупреждение, изменил стандарт на ES7
+console.log(mathProduct);
+// Вывод первых двух цифр
+console.log(String(mathProduct).substr(0, 2)); // Данные в виде строки, можно перевести в тип number с помощью метода
