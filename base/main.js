0bf53cfe85f498dcf875e55331ff3e85271a0b7d
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-06-06 12:45:26 +0300

    fix: delete comments in code, all variables are const

--- base/main.js
+++ base/main.js
@@ -1,24 +1,18 @@
 // Переменные
-let money = 4500, // Тип number
-  income = 'фриланс', // Тип string
-  addExpenses = 'Софт, Подарки', // Тип string
-  deposit = false; // Тип boolean
-const  mission = 100000, // Тип number
-  period = 7; // Тип number
+const money = 4500,
+  income = 'фриланс',
+  addExpenses = 'Софт, Подарки',
+  deposit = false,
+  mission = 100000,
+  period = 7;
 
 // Методы и свойства
-
-// Вывод типов данных
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
-// Выводы длины строки
 console.log(addExpenses.length);
-// Вывод сообщений
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
-// Приведение строки к нижнему регистру и разбиение на массив
 console.log(addExpenses.toLowerCase().split(', '));
-// Новая переменная
 let budgetDay = money / 30;
 console.log(budgetDay);
