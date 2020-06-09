703ca068a8357d323c4fc56c68452bf859cc0cf5
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-06-09 12:46:10 +0300

    feat: add new functional from lesson 3

--- base/main.js
+++ base/main.js
@@ -1,10 +1,19 @@
 // Переменные
-const money = 4500,
+const money = +prompt('Ваш месячный доход?'),
   income = 'фриланс',
-  addExpenses = 'Софт, Подарки',
-  deposit = false,
+  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+  deposit = !!confirm('Есть ли у вас депозит в банке?'),
   mission = 100000,
   period = 7;
+// Обязательные расходы
+const expenses1 = prompt('Введите обязательную статью расходов?'),
+  amount1 = +prompt('Во сколько это обойдется?'),
+  expenses2 = prompt('Введите обязательную статью расходов?'),
+  amount2 = +prompt('Во сколько это обойдется?');
+
+const budgetMonth = money - amount1 - amount2;
+console.log(budgetMonth);
+const budgetDay = Math.floor(budgetMonth / 30);
 
 // Методы и свойства
 console.log(typeof money);
@@ -14,5 +23,14 @@ console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
 console.log(addExpenses.toLowerCase().split(', '));
-const budgetDay = money / 30;
 console.log(budgetDay);
+
+if (budgetDay > 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay > 600 && budgetDay <= 1200) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600 && budgetDay >= 0) {
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} else {
+  console.log('Что-то пошло не так');
+}
