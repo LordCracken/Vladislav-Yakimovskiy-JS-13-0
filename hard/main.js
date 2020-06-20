cf06e498632fb97f7dec31739850db4afe19807b
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-06-20 15:33:41 +0300

    frefact: change methods for numeric data, rewrite function addZero, add words arrays

--- hard/main.js
+++ hard/main.js
@@ -1,6 +1,11 @@
 'use strict';
-const outputDate = document.createElement('p'),
-  getDeclension = (num, words) => {
+const outputDate = document.createElement('p');
+
+const hoursWords = ['час', 'часа', 'часов'],
+  minutesWords = ['минута', 'минуты', 'минут'],
+  secondsWords = ['секунда', 'секунды', 'секунд'];
+
+const getDeclension = (num, words) => {
     const remainder = num % 10;
     if (remainder === 1) {
       return num + ' ' + words[0];
@@ -11,15 +16,13 @@ const outputDate = document.createElement('p'),
     }
   },
   addZero = (num) => {
-    if (Math.trunc(num / 10) === 0) {
-      return '0' + num;
-    } else {
-      return num;
-    }
+    return (Math.trunc(num / 10) === 0) ?  '0' + num : num;
   };
+
 let date = new Date(),
   stringDate = {},
   numDate = {};
+
 setInterval(() => {
   date = new Date(),
     stringDate = {
@@ -28,48 +31,26 @@ setInterval(() => {
       })[0].toUpperCase() + date.toLocaleString('ru', {
         weekday: 'long'
       }).substr(1),
-      year: date.toLocaleString('ru', {
-        year: 'numeric'
-      }),
-      month: (date.getMonth === 3 || date.getMonth === 8 ? date.toLocaleString('ru', {month: 'long'}) + 'а' : date.toLocaleString('ru', {month: 'long'}).slice(0, -1) + 'я'),
-      day: date.toLocaleString('ru', {
-        day: 'numeric'
-      }),
-      hour: date.toLocaleString('ru', {
-        hour: 'numeric'
-      }),
-      minute: date.toLocaleString('ru', {
-        minute: 'numeric'
-      }),
-      second: date.toLocaleString('ru', {
-        second: 'numeric'
-      })
+      year: date.getFullYear(),
+      month: (date.getMonth() === 3 || date.getMonth() === 8 ? date.toLocaleString('ru', {month: 'long'}) + 'а' : date.toLocaleString('ru', {month: 'long'}).slice(0, -1) + 'я'),
+      day: date.getDate(),
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+      second: date.getSeconds()
     },
     numDate = {
-      year: date.toLocaleString('ru', {
-        year: 'numeric'
-      }),
-      month: date.toLocaleString('ru', {
-        month: 'numeric'
-      }),
-      day: date.toLocaleString('ru', {
-        day: 'numeric'
-      }),
-      hour: date.toLocaleString('ru', {
-        hour: 'numeric'
-      }),
-      minute: date.toLocaleString('ru', {
-        minute: 'numeric'
-      }),
-      second: date.toLocaleString('ru', {
-        second: 'numeric'
-      })
+      year: date.getFullYear(),
+      month: date.getMonth(),
+      day: date.getDate(),
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+      second: date.getSeconds()
     };
 }, 1000);
 
 const outputSringDate = document.querySelector('.string-date'),
   outputNumDate = document.querySelector('.num-date');
 setInterval(() => {
-  outputSringDate.textContent = `Сегодня ${stringDate.weekday}, ${stringDate.day} ${stringDate.month} ${stringDate.year} года, ${getDeclension(stringDate.hour, ['час', 'часа', 'часов'])} ${getDeclension(stringDate.minute, ['минута', 'минуты', 'минут'])} ${getDeclension(stringDate.second, ['секунда', 'секунды', 'секунд'])}`;
+  outputSringDate.textContent = `Сегодня ${stringDate.weekday}, ${stringDate.day} ${stringDate.month} ${stringDate.year} года, ${getDeclension(stringDate.hour, hoursWords)} ${getDeclension(stringDate.minute, minutesWords)} ${getDeclension(stringDate.second, secondsWords)}`;
   outputNumDate.textContent = `${addZero(numDate.day)}.${addZero(numDate.month)}.${addZero(numDate.year)} - ${numDate.hour}:${addZero(numDate.minute)}:${addZero(numDate.second)}`;
 }, 1000);
