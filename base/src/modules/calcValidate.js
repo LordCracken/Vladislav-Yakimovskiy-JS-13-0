7c0f4d3fa4b5579aaa035c3ab36d6bd16ffa415e
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-07-22 15:19:00 +0300

    feat: add base instruments for project

--- base/src/modules/calcValidate.js
+++
@@ -1,8 +0,0 @@
-const calcValidate = () => {
-  document.documentElement.addEventListener('input', event => {
-    const target = event.target;
-    if (target.matches('input.calc-item')) target.value = target.value.replace(/\D/g, '');
-  });
-};
-
-export default calcValidate;
