a03e10d7bf3dbe1e1105f4a8f31cca09ec532abe
LordCracken <vladislav.yakimovskiy@mail.ru>
2020-07-22 18:12:49 +0300

    feat: add smooth scroll

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 // eslint-disable-next-line strict
 'use strict';
 
+import '@babel/polyfill';
 import 'element-closest-polyfill';
 import 'dom-node-polyfills';
+import smoothscroll from 'smoothscroll-polyfill';
+smoothscroll.polyfill();
 
 import toggleMenu from './modules/toggleMenu';
+import smoothScroll from './modules/smoothScroll';
 
 toggleMenu();
+smoothScroll();
---
+++ src/modules/smoothScroll.js
@@ -0,0 +1,13 @@
+const smoothScroll = () => {
+  document.documentElement.addEventListener('click', event => {
+    const target = event.target;
+    if (target.closest('.button-footer')) {
+      document.querySelector(target.querySelector('a').getAttribute('href')).scrollIntoView({ alignToTop: true, behavior: 'smooth' });
+    } else if (target.getAttribute('href') && target.getAttribute('href') !== '#' && target.getAttribute('href')[0] === '#') {
+      const targetPoint = document.querySelector(target.getAttribute('href'));
+      targetPoint.scrollIntoView({ alignToTop: true, behavior: 'smooth' });
+    }
+  });
+};
+
+export default smoothScroll;
